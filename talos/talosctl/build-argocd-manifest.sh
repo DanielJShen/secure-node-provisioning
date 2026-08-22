099b67f6b078770bb38901b730d636079d4ff74b
DanielJShen <6959648+DanielJShen@users.noreply.github.com>
2026-08-22 21:30:03 +0100

    Talosctl provision script fix

--- talos/talosctl/build-argocd-manifest.sh
+++ talos/talosctl/build-argocd-manifest.sh
@@ -4,7 +4,8 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 KUSTOMIZATION_FILE="${SCRIPT_DIR}/build/kustomize/kustomization.yaml"
 KUSTOMIZED_MANIFEST_FILE="${SCRIPT_DIR}/build/kustomize/argocd-install-namespaced.yaml"
-ADDITIONAL_ARGO_MANIFESTS_FILE="${SCRIPT_DIR}/build/kustomize/argo-manifests.yaml"
+SECRETS_FILE="${SCRIPT_DIR}/build/kustomize/secrets.yaml"
+INITIAL_ARGO_APPS_FILE="${SCRIPT_DIR}/argocd-manifests.yaml"
 OUTPUT_MANIFEST_FILE="${SCRIPT_DIR}/build/kustomize/argocd-manifest-final.yaml"
 
 # Create the kustomize dir if it doesn't exist
@@ -20,7 +21,7 @@ if [[ "${prompt1}" =~ [yY](es)* ]]; then
   NC='\033[0m'
   echo -e "${BLUE}###\nAdd this SSH Key to your git repository:\n$(cat "${SCRIPT_DIR}/build/kustomize/key.pub")\n###${NC}"
 
-  cat <<EOF | sed -e 's/^/  /' > "${ADDITIONAL_ARGO_MANIFESTS_FILE}"
+  cat <<EOF | sed -e 's/^/  /' > "${SECRETS_FILE}"
 apiVersion: v1
 kind: Secret
 metadata:
@@ -36,20 +37,23 @@ $(cat "${SCRIPT_DIR}/build/kustomize/key" | sed -e 's/^/    /')
 EOF
 fi
 
-# Use Kustomize to set the namespace throughout the manifest
+# Use Kustomize to set the namespace throughout the manifest/s
+INITIAL_ARGO_APPS_LOCAL_FILE="${SCRIPT_DIR}/build/kustomize/initial-argo-apps.yaml"
+cp "${INITIAL_ARGO_APPS_FILE}" "${INITIAL_ARGO_APPS_LOCAL_FILE}"
+
 cat <<EOF > "${KUSTOMIZATION_FILE}"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 namespace: argocd
 resources:
   - https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-  - ${ADDITIONAL_ARGO_MANIFESTS_FILE}
+  - ${INITIAL_ARGO_APPS_LOCAL_FILE}
 EOF
 
 kubectl kustomize "$(dirname "${KUSTOMIZATION_FILE}")" | sed -e 's/^/  /' > "${KUSTOMIZED_MANIFEST_FILE}"
 
 # Prepend a namespace manifest
-cat <<EOF | cat - "${KUSTOMIZED_MANIFEST_FILE}" <(echo "  ---") "${ADDITIONAL_ARGO_MANIFESTS_FILE}"  > "${OUTPUT_MANIFEST_FILE}"
+cat <<EOF | cat - "${KUSTOMIZED_MANIFEST_FILE}" <(echo "  ---") "${SECRETS_FILE}"  > "${OUTPUT_MANIFEST_FILE}"
 apiVersion: v1alpha1
 kind: KubeInlineManifestConfig
 name: argocd-install
